f72d6ca9bdb9a6ee2cb6a14ff563709835c9fe69
C Bindhu Madhava Varma <54420831+BINDHUMADHAVAVARMA@users.noreply.github.com>
2021-05-29 13:07:29 +0530

    Delete lib directory

--- lib/Transaction.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-class Transaction{
-  @required String id;
-  @required String title;
-  @required double amount;
-  @required DateTime date;
-  Transaction({this.id,this.title,this.amount,this.date});
-}
